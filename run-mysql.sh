558c629383341edde9a8675bdfba30bb9de29a7b
ZZ3n <ndsrhkd@naver.com>
2024-04-10 21:13:06 +0900

    setup run-mysql.sh

--- run-mysql.sh
+++ run-mysql.sh
@@ -5,5 +5,8 @@ docker build -t gonggong-db-image .
 echo "### Docker Run ###"
 docker run -d --name gonggong-db -p 3306:3306 -d gonggong-db-image
 
+cp ./secrets.json ./secrets.json.backup
+cp ./secrets-local.json ./secrets.json
+
 echo "### RUN MIGRATE ###"
 python3 manage.py migrate
