5c74d17deae29c85f692fa8451483df7029bb466
nooreong2 <nooreong0503@gmail.com>
2024-04-14 22:43:10 +0900

    test parking service

--- test/urls.py
+++ test/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('test1', views.test1),
-    path('test2', views.test2),
-    path('test3', views.test3),
-    path('geocode', views.geocode_test),
+    path("test1", views.test1),
+    path("test2", views.test2),
+    path("test3", views.test3),
+    path("test4", views.test4),
+    path("geocode", views.geocode_test),
 ]
--- test/views.py
+++ test/views.py
@@ -75,8 +75,21 @@ def test2(request):
 
 # 서울시립미술관 교육 정보 (교육 프로그램, 교육 기간 대략 1 달 정도)
 def test3(request):
-    api_key = ""
+    api_key = SEOUL_API_KEY
     api_url = f"http://openapi.seoul.go.kr:8088/{api_key}/json/ListEducationOfSeoulMOAInfo/1/5/"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        return JsonResponse(response.json())
+    else:
+        error_message = f"Failed to fetch data. Status code: {response.status_code}"
+        return JsonResponse({"error_message": error_message}, status=500)
+
+
+# 서울시 공영주차장
+def test4(request):
+    api_key = SEOUL_API_KEY
+    api_url = f"http://openapi.seoul.go.kr:8088/{api_key}/json/GetParkInfo/1/5/"
     response = requests.get(api_url)
 
     if response.status_code == 200:
