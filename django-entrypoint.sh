a8bed4a170ec2249109b2b8719d7f2423b99d5b8
ZZ3n <ndsrhkd@naver.com>
2024-04-20 13:53:34 +0900

    add superuser

--- django-entrypoint.sh
+++ django-entrypoint.sh
@@ -3,5 +3,11 @@
 echo "### Migration ###"
 python manage.py migrate
 
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=zz3n.dev@gmail.com
+export DJANGO_SUPERUSER_PASSWORD=secret
+
+python manage.py createsuperuser --no-input
+
 echo "### Run Server ###"
 python manage.py runserver 0.0.0.0:8000
