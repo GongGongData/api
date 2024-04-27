346f593130d973990b8cc93180b00bc60b59e3e1
ZZ3n <ndsrhkd@naver.com>
2024-04-27 18:49:01 +0900

    redirect

--- gonggongapp/urls.py
+++ gonggongapp/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import RedirectView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -43,6 +44,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns += [
+    path('', RedirectView.as_view(url="/swagger")),
     re_path('^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0),
             name='schema-json'),
